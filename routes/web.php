061827c4da6f09696a7685bfea4341bac55e1135
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-11-16 13:02:45 +0800

    練習 6-2-1 取得 Model ( $fourthPost )

--- routes/web.php
+++ routes/web.php
@@ -54,9 +54,11 @@ Route::get('/', function () {
 //    $allPosts = Post::all();
 //    dd($allPosts);
 
-    $featuredPosts = Post::where('is_feature',1)->get();
-    dd($featuredPosts);
+//    $featuredPosts = Post::where('is_feature',1)->get();
+//    dd($featuredPosts);
 
+    $fourthPost=Post::find(4);
+    dd($fourthPost);
 });
 
 
